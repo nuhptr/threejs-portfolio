3b5a33a7d0b05980fbe5845cf04bf4f5d505b6d9
nuhptr <nugrahaadi733@gmail.com>
2023-10-23 19:51:39 +0700

    app: Navbar, and Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import React from "react"
+import { BrowserRouter } from "react-router-dom"
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
 
 export default function App() {
-   return <div>App</div>
+   return (
+      <BrowserRouter>
+         <div className="relative z-0 bg-primary">
+            <div className="bg-center bg-no-repeat bg-cover bg-hero-pattern">
+               <Navbar />
+               <Hero />
+            </div>
+         </div>
+      </BrowserRouter>
+   )
 }
