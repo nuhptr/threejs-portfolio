dd26e1a8abb698b1df7211cfc5b98b6fd25802e8
nuhptr <nugrahaadi733@gmail.com>
2023-10-24 12:44:14 +0700

    update constant

--- src/constant/index.js
+++ src/constant/index.js
@@ -33,7 +33,7 @@ export const navLinks = [
 
 const services = [
    { title: "Web Developer", icon: web },
-   { title: "React Native Developer", icon: mobile },
+   { title: "UI UX Designer", icon: mobile },
    { title: "Backend Developer", icon: backend },
    { title: "Content Creator", icon: creator },
 ]
