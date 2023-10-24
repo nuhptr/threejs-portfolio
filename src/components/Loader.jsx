2d7d728b28cad1fbab556be64f9dc68b49056219
nuhptr <nugrahaadi733@gmail.com>
2023-10-24 12:44:38 +0700

    comp: update loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,13 @@
+import { Html, useProgress } from "@react-three/drei"
 import React from "react"
 
 export default function Loader() {
-   return <div>Loader</div>
+   const { progress } = useProgress()
+
+   return (
+      <Html>
+         <span className="canvas-load"></span>
+         <p style={{ fontSize: 14, color: "#f1f1f1", fontWeight: 800, marginTop: 40 }}>{progress.toFixed(2)}%</p>
+      </Html>
+   )
 }
