5cfa92ef90f1aa5d569b953b0c553fba6327b376
nuhptr <nugrahaadi733@gmail.com>
2023-10-25 00:09:13 +0700

    app: update import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom"
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works } from "./components"
+import { StarsCanvas } from "./components/canvas"
 
 export default function App() {
    return (
