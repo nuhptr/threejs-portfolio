b33b5d9775cac6cbb403e4090b339f09e271d23b
nuhptr <nugrahaadi733@gmail.com>
2023-10-24 12:44:01 +0700

    update motion utilities

--- src/utilities/motion.js
+++ src/utilities/motion.js
@@ -1,66 +1,47 @@
 export const textVariant = (delay) => {
    return {
       hidden: { y: -50, opacity: 0 },
-      show: {
-         y: 0,
-         opacity: 1,
-         transition: { type: "spring", duration: 1.25, delay: delay },
-      },
+      show: { y: 0, opacity: 1, transition: { type: "spring", duration: 1.25, delay: delay } },
    }
 }
 
-const directionRight = direction === "right" ? -100 : 0
-const directionDown = direction === "down" ? -100 : 0
-
 export const fadeIn = (direction, type, delay, duration) => {
+   const directionRight = direction === "right" ? -100 : 0
+   const directionDown = direction === "down" ? -100 : 0
+
    return {
       hidden: {
          x: direction === "left" ? 100 : directionRight,
          y: direction === "up" ? 100 : directionDown,
          opacity: 0,
       },
-      show: {
-         x: 0,
-         y: 0,
-         opacity: 1,
-         transition: { type: type, delay: delay, duration: duration, ease: "easeOut" },
-      },
+      show: { x: 0, y: 0, opacity: 1, transition: { type: type, delay: delay, duration: duration, ease: "easeOut" } },
    }
 }
 
 export const zoomIn = (delay, duration) => {
    return {
       hidden: { scale: 0, opacity: 0 },
-      show: {
-         scale: 1,
-         opacity: 1,
-         transition: { type: "tween", delay: delay, duration: duration, ease: "easeOut" },
-      },
+      show: { scale: 1, opacity: 1, transition: { type: "tween", delay: delay, duration: duration, ease: "easeOut" } },
    }
 }
 
-const directionRightPersen = direction === "right" ? "100%" : 0
-const directionDownPersen = direction === "down" ? "100%" : 0
-
 export const slideIn = (direction, type, delay, duration) => {
+   const directionRightPersen = direction === "right" ? "100%" : 0
+   const directionDownPersen = direction === "down" ? "100%" : 0
+
    return {
       hidden: {
          x: direction === "left" ? "-100%" : directionRightPersen,
          y: direction === "up" ? "100%" : directionDownPersen,
       },
-      show: {
-         x: 0,
-         y: 0,
-         transition: { type: type, delay: delay, duration: duration, ease: "easeOut" },
-      },
+      show: { x: 0, y: 0, transition: { type: type, delay: delay, duration: duration, ease: "easeOut" } },
    }
 }
 
 export const staggerContainer = (staggerChildren, delayChildren) => {
    return {
       hidden: {},
-      show: {
-         transition: { staggerChildren: staggerChildren, delayChildren: delayChildren || 0 },
-      },
+      show: { transition: { staggerChildren: staggerChildren, delayChildren: delayChildren || 0 } },
    }
 }
