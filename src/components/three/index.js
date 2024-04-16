52d201a5b8126b13cb97078b5097d1aea2af38d7
nuhptr <nugrahaadi733@gmail.com>
2024-04-16 22:26:15 +0700

    three: components update code

---
+++ src/components/three/index.js
@@ -0,0 +1,6 @@
+import BallCanvas from "./ball"
+import ComputersCanvas from "./computers"
+import EarthCanvas from "./earth"
+import StarsCanvas from "./starts"
+
+export { BallCanvas, ComputersCanvas, EarthCanvas, StarsCanvas }
