3456ada2025d97804eaed9aeb4906c4cabd3b2ea
nuhptr <nugrahaadi733@gmail.com>
2024-04-16 21:09:03 +0700

    assets: update name

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,62 +1,62 @@
-import logo from "./logo.svg"
-import backend from "./backend.png"
-import creator from "./creator.png"
-import mobile from "./mobile.png"
-import web from "./web.png"
-import github from "./github.png"
-import menu from "./menu.svg"
-import close from "./close.svg"
+import iconLogo from "./ic_logo.svg"
+import iconMenu from "./ic_menu.svg"
+import iconClose from "./ic_close.svg"
+import imgBackend from "./img_backend.png"
+import imgCreator from "./img_creator.png"
+import imgMobile from "./img_mobile.png"
+import imgWeb from "./img_web.png"
+import imgGithub from "./img_github.png"
 
-import css from "./tech/css.png"
-import docker from "./tech/docker.png"
-import figma from "./tech/figma.png"
-import git from "./tech/git.png"
-import html from "./tech/html.png"
-import javascript from "./tech/javascript.png"
-import mongodb from "./tech/mongodb.png"
-import nodejs from "./tech/nodejs.png"
-import reactjs from "./tech/reactjs.png"
-import redux from "./tech/redux.png"
-import tailwind from "./tech/tailwind.png"
-import typescript from "./tech/typescript.png"
-import threejs from "./tech/threejs.svg"
+import imgCss from "./tech/img_css.png"
+import imgDocker from "./tech/img_docker.png"
+import imgFigma from "./tech/img_figma.png"
+import imgGit from "./tech/img_git.png"
+import imgHtml from "./tech/img_html.png"
+import imgJavascript from "./tech/img_javascript.png"
+import imgMongodb from "./tech/img_mongodb.png"
+import imgNode from "./tech/img_nodejs.png"
+import imgReact from "./tech/img_reactjs.png"
+import imgRedux from "./tech/img_redux.png"
+import imgTailwind from "./tech/img_tailwind.png"
+import imgTypescript from "./tech/img_typescript.png"
+import iconThreejs from "./tech/ic_threejs.svg"
 
-import meta from "./company/meta.png"
-import shopify from "./company/shopify.png"
-import starbucks from "./company/starbucks.png"
-import tesla from "./company/tesla.png"
+import imgMeta from "./company/img_meta.png"
+import imgShopify from "./company/img_shopify.png"
+import imgStarbucks from "./company/img_starbucks.png"
+import imgTesla from "./company/img_tesla.png"
 
-import carrent from "./carrent.png"
-import jobit from "./jobit.png"
-import tripguide from "./tripguide.png"
+import imgCarrent from "./img_carrent.png"
+import imgJobbit from "./img_jobit.png"
+import imgTripGuide from "./img_tripguide.png"
 
 export {
-   logo,
-   backend,
-   creator,
-   mobile,
-   web,
-   github,
-   menu,
-   close,
-   css,
-   docker,
-   figma,
-   git,
-   html,
-   javascript,
-   mongodb,
-   nodejs,
-   reactjs,
-   redux,
-   tailwind,
-   typescript,
-   threejs,
-   meta,
-   shopify,
-   starbucks,
-   tesla,
-   carrent,
-   jobit,
-   tripguide,
+   iconLogo,
+   imgBackend,
+   imgCreator,
+   imgMobile,
+   imgWeb,
+   imgGithub,
+   iconMenu,
+   iconClose,
+   imgCss,
+   imgDocker,
+   imgFigma,
+   imgGit,
+   imgHtml,
+   imgJavascript,
+   imgMongodb,
+   imgNode,
+   imgReact,
+   imgRedux,
+   imgTailwind,
+   imgTypescript,
+   iconThreejs,
+   imgMeta,
+   imgShopify,
+   imgStarbucks,
+   imgTesla,
+   imgCarrent,
+   imgJobbit,
+   imgTripGuide,
 }
