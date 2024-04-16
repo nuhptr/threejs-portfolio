edfd758783e2f47c7ff4afd71d91718eb5bad25f
nuhptr <nugrahaadi733@gmail.com>
2024-04-16 22:23:19 +0700

    utils: new update code

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -1,11 +1,11 @@
-export const textVariant = (delay) => {
+export function textVariant(delay) {
    return {
       hidden: { y: -50, opacity: 0 },
       show: { y: 0, opacity: 1, transition: { type: "spring", duration: 1.25, delay: delay } },
    }
 }
 
-export const fadeIn = (direction, type, delay, duration) => {
+export function fadeIn(direction, type, delay, duration) {
    const directionRight = direction === "right" ? -100 : 0
    const directionDown = direction === "down" ? -100 : 0
 
@@ -15,18 +15,27 @@ export const fadeIn = (direction, type, delay, duration) => {
          y: direction === "up" ? 100 : directionDown,
          opacity: 0,
       },
-      show: { x: 0, y: 0, opacity: 1, transition: { type: type, delay: delay, duration: duration, ease: "easeOut" } },
+      show: {
+         x: 0,
+         y: 0,
+         opacity: 1,
+         transition: { type: type, delay: delay, duration: duration, ease: "easeOut" },
+      },
    }
 }
 
-export const zoomIn = (delay, duration) => {
+export function zoomIn(delay, duration) {
    return {
       hidden: { scale: 0, opacity: 0 },
-      show: { scale: 1, opacity: 1, transition: { type: "tween", delay: delay, duration: duration, ease: "easeOut" } },
+      show: {
+         scale: 1,
+         opacity: 1,
+         transition: { type: "tween", delay: delay, duration: duration, ease: "easeOut" },
+      },
    }
 }
 
-export const slideIn = (direction, type, delay, duration) => {
+export function slideIn(direction, type, delay, duration) {
    const directionRightPersen = direction === "right" ? "100%" : 0
    const directionDownPersen = direction === "down" ? "100%" : 0
 
@@ -35,11 +44,15 @@ export const slideIn = (direction, type, delay, duration) => {
          x: direction === "left" ? "-100%" : directionRightPersen,
          y: direction === "up" ? "100%" : directionDownPersen,
       },
-      show: { x: 0, y: 0, transition: { type: type, delay: delay, duration: duration, ease: "easeOut" } },
+      show: {
+         x: 0,
+         y: 0,
+         transition: { type: type, delay: delay, duration: duration, ease: "easeOut" },
+      },
    }
 }
 
-export const staggerContainer = (staggerChildren, delayChildren) => {
+export function staggerContainer(staggerChildren, delayChildren) {
    return {
       hidden: {},
       show: { transition: { staggerChildren: staggerChildren, delayChildren: delayChildren || 0 } },
